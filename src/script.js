bfe30f92327032a7d82f2d5706eb7ff339c1c6de
Gabrieups <paixaogabriel498@gmail.com>
2023-11-15 17:27:42 -0300

    iohñiophjñiophj

--- src/script.js
+++ src/script.js
@@ -1,3 +1,32 @@
+const userbtn = document.getElementById("UserBtn");
+const toolbtn = document.getElementById("ToolBtn");
+const uparameter = document.getElementById("mainUser");
+const tparameter = document.getElementById("mainTool");
+
+function userAct(){
+    const tguide = document.getElementById("guideT");
+    const uguide = document.getElementById("guideU");
+
+    tparameter.style.display = "none";
+    uparameter.style.display = "block";
+    tguide.style.display = "none";
+    uguide.style.display = "block";
+}
+
+function toolAct(){
+    const tguide = document.getElementById("guideT");
+    const uguide = document.getElementById("guideU");
+
+    tparameter.style.display = "block";
+    uparameter.style.display = "none";
+    tguide.style.display = "block";
+    uguide.style.display = "none";
+
+}
+userbtn.addEventListener('click', userAct);
+toolbtn.addEventListener('click', toolAct);
+
+
 const ubuscabtn = document.getElementById("UBuscaBtn");
 const ucadbtn = document.getElementById("uCadBtn");
 
@@ -14,4 +43,18 @@ function userCadAtc(){
 
 }
 
+function userBuscaAtc(){
+    const ubuscaparameter = document.getElementById("UserBuscaParamater");
+    const ucadparameter = document.getElementById("UserCadParamater");
+    ubuscabtn.style.backgroundColor = "#fff";
+    ubuscabtn.style.color = "#000";
+    ubuscabtn.style.zIndex = 1;
+    ucadbtn.style.backgroundColor = "#d0ccd3";
+    ucadbtn.style.color = "#5b5b5b";
+    ubuscaparameter.style.display = "block";
+    ucadparameter.style.display = "none";
+
+}
+
 ucadbtn.addEventListener('click', userCadAtc);
+ubuscabtn.addEventListener('click', userBuscaAtc);
